f8a573adf49c1d592810d449b4f2f5294a338cd2
Yonsei-MILab <52816867+Yonsei-MILab@users.noreply.github.com>
2024-05-27 00:49:44 +0900

    Update 2_Simultaneous_Training_Procedure.py

--- 2_Simultaneous_Training_Procedure.py
+++ 2_Simultaneous_Training_Procedure.py
@@ -6,7 +6,7 @@ from scipy.io import loadmat
 import matplotlib.pyplot as plt
 import torch
 from skimage.metrics import normalized_root_mse as nrmse, structural_similarity as ssim, peak_signal_noise_ratio as psnr
-from util import configure
+from Util import configure
 from Util_Phase_Only_Joint import laplacian_fn, mag_weight_fn, neural_weight_fn, build_net, train_net
 from torch.optim.lr_scheduler import StepLR
 from tqdm import tqdm
