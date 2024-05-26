e2ae9735d740c5a3c692e12c1f7fbee3d3da0932
Yonsei-MILab <52816867+Yonsei-MILab@users.noreply.github.com>
2024-05-27 00:42:25 +0900

    Update 3_Testing_Procedure.py

--- 3_Testing_Procedure.py
+++ 3_Testing_Procedure.py
@@ -89,8 +89,8 @@ phase_test = torch.angle(imgs_test)
 mag_test = torch.abs(imgs_test)
 
 # Test for Axial Plane
-DL_result_Axial = []
-for idx in range(imgs3.size(0)):
+DL_result_axial = []
+for idx in range(imgs_test.size(0)):
     imgs_idx = imgs_test[idx]
     mask_idx = mag_test[idx]
     phase_idx = torch.angle(imgs_idx)
@@ -102,13 +102,65 @@ for idx in range(imgs3.size(0)):
     with torch.no_grad():
         cond_nn = lap_fn(phase_idx /2, neural_weight_fn(mag_idx, mask_idx, net1, mean_idx, std_idx)) / muwf
         DL_result = torch.where(cond_nn > 0, cond_nn2, lap_fn(phase_idx / 2, mag_weight_fn(mag_idx, mask_idx, sigma=0.2)) / muwf)
-        DL_result_Axial.append(DL_result)
-DL_result_Axial = torch.stack(DL_result_Axial) 
+        DL_result_axial.append(DL_result)
+DL_result_axial = torch.stack(DL_result_axial) 
 
 
+# Test for Coronal Plane
+masks_test_coronal = masks_test.permute(1,0,2)
+imgs_test_coronal = imgs_test.permute(1,0,2)
+
+phase_test_coronal = torch.angle(imgs_test_coronal)
+mag_test_coronal = torch.abs(imgs_test_coronal)
+
+DL_result_coronal = []
+DL_result = []
+for idx in range(imgs_test_coronal.size(0)):
+    imgs_idx_coronal = imgs_test_coronal[idx]
+    mask_idx_coronal = mag_test_coronal[idx]
+    phase_idx_coronal = torch.angle(imgs_idx_coronal)
+    mag_idx_coronal = torch.abs(imgs_idx_coronal)
+    
+    lap_fn = laplacian_fn(mask_idx_coronal, kernel_size, res)
+    mean_idx_coronal, std_idx_coronal = torch.mean(mag_idx_coronal), torch.std(mag_idx_coronal)
+    
+    with torch.no_grad():
+        cond_nn_coronal = lap_fn(phase_idx_coronal /2, neural_weight_fn(mag_idx_coronal, mask_idx_coronal, net2, mean_idx_coronal, std_idx_coronal)) / muwf
+        DL_result = torch.where(cond_nn > 0, cond_nn2_coronal, lap_fn(phase_idx_coronal / 2, mag_weight_fn(mag_idx_coronal, mask_idx_coronal, sigma=0.2)) / muwf)
+        DL_result_coronal.append(DL_result)
+DL_result_coronal = torch.stack(DL_result_coronal) 
+DL_result_coronal = DL_result_coronal.permute(1,0,2)
 
 
+# Test for Sagittal Plane
+masks_test_sagittal = masks_test.permute(2,0,1)
+imgs_test_sagittal = imgs_test.permute(2,0,1)
 
+phase_test_sagittal = torch.angle(imgs_test_sagittal)
+mag_test_sagittal = torch.abs(imgs_test_sagittal)
+
+DL_result_sagittal = []
+DL_result = []
+for idx in range(imgs_test_sagittal.size(0)):
+    imgs_idx_sagittal = imgs_test_sagittal[idx]
+    mask_idx_sagittal = mag_test_sagittal[idx]
+    phase_idx_sagittal = torch.angle(imgs_idx_sagittal)
+    mag_idx_sagittal = torch.abs(imgs_idx_sagittal)
+    
+    lap_fn = laplacian_fn(mask_idx_sagittal, kernel_size, res)
+    mean_idx_sagittal, std_idx_sagittal = torch.mean(mag_idx_sagittal), torch.std(mag_idx_sagittal)
+    
+    with torch.no_grad():
+        cond_nn_sagittal = lap_fn(phase_idx_sagittal /2, neural_weight_fn(mag_idx_sagittal, mask_idx_sagittal, net3, mean_idx_sagittal, std_idx_sagittal)) / muwf
+        DL_result = torch.where(cond_nn > 0, cond_nn_sagittal, lap_fn(phase_idx_sagittal / 2, mag_weight_fn(mag_idx_sagittal, mask_idx_sagittal, sigma=0.2)) / muwf)
+        DL_result_sagittal.append(DL_result)
+DL_result_sagittal = torch.stack(DL_result_sagittal) 
+DL_result_sagittal = DL_result_sagittal.permute(1,2,0)
+
+DL_result_coupled = (DL_result_axial + DL_result_coronal + DL_result_sagittal) / 3
+
+# 1, 2, 0 => 2, 0, 1
+# 2, 0, 1 -> 1, 2, 0
 # Prepare dataset
 phases, mags = torch.angle(imgs), torch.abs(imgs)
 mean, std = torch.mean(mags), torch.std(mags)
